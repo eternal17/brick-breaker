2a6b6534663397624a7db114c05579acaa3e2623
eternal17 <sarmadkhatri15@gmail.com>
2022-07-01 14:27:09 +0100

    pad size and position added

---
+++ game.js
@@ -0,0 +1,9 @@
+const pad = document.createElement('div')
+pad.classList.add('pad')
+
+
+// document.body.append(pad)
+
+const gameBoard = document.getElementById('game-board')
+
+gameBoard.appendChild(pad)
