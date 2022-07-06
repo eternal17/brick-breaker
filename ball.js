4f1ecaac114fd77ee6043ab043f45fb29cdcd2ed
tb38r <tlawal86@hotmail.co.uk>
2022-07-06 20:39:04 +0100

    backtogrid

--- ball.js
+++ ball.js
@@ -1,6 +1,9 @@
 import { hasSpaceBeenPressed as beenPressed } from "./pad.js";
 
-let collided = false
+let collidedRight = false;
+let collidedTop = false;
+let collidedLeft = false;
+let collidedPad = false;
 
 console.log("been", beenPressed);
 
@@ -16,14 +19,17 @@ function moveBall(gameBoard) {
   let ballColumn = parseInt(ballValues.gridColumnStart);
   let ballRow = parseInt(ballValues.gridRowStart);
 
-  if (beenPressed.ball & !collided) {
-    ball.style.gridColumnStart = ballColumn + 1;
-    ball.style.gridRowStart = ballRow - 1;
+  if (beenPressed.ball & !collidedRight & !collidedTop & !collidedLeft) {
+   ball.style.gridColumnStart = ballColumn +1;
+    ball.style.gridRowStart = ballRow  -1;
   }
 }
 
 function checkWallCollision() {
   let ball = document.querySelector(".ball");
+  let paddle = document.querySelector(".pad");
+  let padRect = paddle.getBoundingClientRect()
+
   let ballValues = window.getComputedStyle(ball);
   let ballColumn = parseInt(ballValues.gridColumnStart);
   let ballRow = parseInt(ballValues.gridRowStart);
@@ -36,18 +42,59 @@ function checkWallCollision() {
   let mainball = document.querySelector(".ball");
 
   let ballRect = mainball.getBoundingClientRect();
-  // console.log('ballTop', ballRect.top);
-  // console.log('ballRight', ballRect.right);
-  // console.log('ballLeft', ballRect.left);
-  // console.log('ballBottom', ballRect.bottom);
+
   if (ballRect.right.toFixed(2) == boardRect.right.toFixed(2)) {
-    collided = true;
-      }
-      
-      if (collided){//change direction
+    
+    collidedRight = true;
+    collidedTop = false;
+    collidedLeft = false;
+    collidedPad = false;
+  }
+
+  if (collidedRight) {
+    //change direction
     ball.style.gridColumnStart = ballColumn - 1;
     ball.style.gridRowStart = ballRow - 1;
   }
+
+  if (ballRect.top.toFixed(2) == boardRect.top.toFixed(2)) {
+    collidedRight = false;
+    collidedTop = true;
+    collidedLeft = false;
+    collidedPad = false;
+  }
+
+  if (collidedTop) {
+    //change direction
+   ball.style.gridColumnStart = ballColumn - 1;
+    ball.style.gridRowStart = ballRow +1;
+  }
+
+  if (ballRect.left.toFixed(2) == boardRect.left.toFixed(2)) {
+    collidedRight = false;
+    collidedTop = false;
+    collidedLeft = true;
+    collidedPad = false;
+  }
+
+  if (collidedLeft) {
+    //change direction
+   ball.style.gridColumnStart = ballColumn +1;
+    ball.style.gridRowStart = ballRow +1;
+  }
+
+
+
+
+
+//   if(gameover){
+//   if (ballRect.bottom.toFixed(2) > padRect.top.toFixed(2)) {
+//     //document.location.reload()
+   
+//   }
+// }
+  
+
 }
 
 export { drawBall, moveBall, checkWallCollision };
