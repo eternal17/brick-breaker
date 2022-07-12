fcfff2a0bb12648804c6dccbba66fc249f19eaf6
tb38r <tlawal86@hotmail.co.uk>
2022-07-12 17:25:12 +0100

    pad moving left and right

--- game.js
+++ game.js
@@ -1,36 +1,54 @@
 const gameBoard = document.querySelector(".game-board");
-const gameBoardRect = gameBoard.getBoundingClientRect()
+const gameBoardRect = gameBoard.getBoundingClientRect();
+const gameCompStyles = window.getComputedStyle(gameBoard);
 
-const paddle_width = 100
-const paddle_margin_bottom = 50
-const paddle_height = 20
+const paddle_width = 100;
+const paddle_margin_bottom = 50;
+const paddle_height = 20;
+
+const pad = document.createElement("div");
+const padCompStyles = window.getComputedStyle(pad);
 
 const paddle = {
-  // x value is 
+  // x value is
   x: gameBoardRect.width / 2 - paddle_width / 2,
   // y value is the top left corner value of the paddle
   y: gameBoardRect.height - paddle_margin_bottom - paddle_height,
   width: paddle_width,
   height: paddle_height,
-  xMovement: 5
-}
-// draw paddle 
+  xMovement: 6,
+};
+
+// draw paddle
 function drawPaddle() {
-  let pad = document.createElement('div')
-  pad.classList.add('pad')
-  pad.style.left = paddle.x + 'px'
-  pad.style.top = paddle.y + 'px'
-  pad.style.width = paddle.width + 'px'
-  pad.style.height = paddle.height + 'px'
-  pad.style.backgroundColor = 'red'
-  pad.style.position = 'absolute'
-  gameBoard.append(pad)
+  pad.classList.add("pad");
+  pad.style.left = paddle.x + "px";
+  pad.style.top = paddle.y + "px";
+  pad.style.width = paddle.width + "px";
+  pad.style.height = paddle.height + "px";
+  pad.style.backgroundColor = "red";
+  pad.style.position = "absolute";
+  gameBoard.append(pad);
 }
 
+//move pad, uses gameboard border as a parameter
+function movePaddle() {
+  document.addEventListener("keydown", function (event) {
+    const padRect = pad.getBoundingClientRect();
 
+    if (event.key == "ArrowRight" && padRect.right + parseInt(gameCompStyles.border) < gameBoardRect.right) {
+      paddle.x = parseInt(padCompStyles.left) + paddle.xMovement;
+    } else if (event.key == "ArrowLeft" && padRect.left - parseInt(gameCompStyles.border) > gameBoardRect.left) {
+      paddle.x = parseInt(padCompStyles.left) - paddle.xMovement;
+    }
+  });
+}
 
-// console.log(pad.getBoundingClientRect())
+function gameLoop() {
+  drawPaddle();
+  movePaddle();
 
+  requestAnimationFrame(gameLoop);
+}
 
-
-drawPaddle()
+requestAnimationFrame(gameLoop);
