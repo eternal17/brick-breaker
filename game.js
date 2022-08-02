6443e3b7d92622a603a4bde69f008ac0012b33d8
tb38r <tlawal86@hotmail.co.uk>
2022-08-02 14:34:16 +0100

    pre movepadbool - works up till now

--- game.js
+++ game.js
@@ -1,33 +1,15 @@
+
+import { paddle, drawPaddle, movePaddle, padCollision,pad} from "./paddle.js";
+
 // gameboard variables
 const gameBoard = document.querySelector(".game-board");
 const gameBoardRect = gameBoard.getBoundingClientRect();
 const gameCompStyles = window.getComputedStyle(gameBoard);
 
-// paddle variables
-const paddle_width = 100;
-const paddle_margin_bottom = 50;
-const paddle_height = 20;
-
-const pad = document.createElement("div");
-const padCompStyles = window.getComputedStyle(pad);
-
-let paddle = {
-  // start is used to track the translated values
-  start: 0,
-  x: gameBoardRect.width / 2 - paddle_width / 2,
-  // y value is the top left corner value of the paddle
-  y: gameBoardRect.height - paddle_margin_bottom - paddle_height,
-  width: paddle_width,
-  height: paddle_height,
-  xMovement: 20,
-  right: false,
-  left: false,
-};
 
 // ball variables
 const ballDiv = document.createElement("div");
 const ballRadius = 10;
-const ballCompStyles = window.getComputedStyle(ballDiv);
 const defaultValue = "translate(0,0)";
 ballDiv.setAttribute("transform", defaultValue);
 
@@ -66,23 +48,12 @@ let a = false;
 
 // sounds
 const win_sound = new Audio('./assets/gamewin1.mp3')
-const paddle_hit_sound = new Audio('./assets/paddlehit.mp3')
 const brick_collided_sound = new Audio('./assets/brickhit1.mp3')
 const life_lost_sound = new Audio('./assets/life-lost.mp3')
 const game_over_sound = new Audio('./assets/game-over.mp3')
 const game_music = new Audio('./assets/game-music.mp3')
 
-// draw paddle
-function drawPaddle() {
-  pad.classList.add("pad");
-  pad.style.left = paddle.x + "px";
-  pad.style.top = paddle.y + "px";
-  pad.style.width = paddle.width + "px";
-  pad.style.height = paddle.height + "px";
-  pad.style.backgroundColor = "white";
-  pad.style.position = "absolute";
-  gameBoard.append(pad);
-}
+
 
 //toggles boolean used within movePaddle function
 function movePaddleBool() {
@@ -96,44 +67,6 @@ function movePaddleBool() {
       paddle.left = true;
     }
   });
-}
-
-//translates paddle on the X axis
-function movePaddle() {
-  const padRect = pad.getBoundingClientRect();
-
-  if (paddle.right) {
-    if (padRect.right + paddle.xMovement <= gameBoardRect.right) {
-      paddle.start += paddle.xMovement;
-    } else {
-      paddle.start += gameBoardRect.right - padRect.right;
-    }
-
-    if (!game_started) {
-      ballDiv.style.transform = `translateX(${paddle.start}px)`;
-      pad.style.transform = `translateX(${paddle.start}px)`;
-      paddle.right = false;
-    }
-
-    pad.style.transform = `translate(${paddle.start}px)`;
-    paddle.right = false;
-  }
-
-  if (paddle.left) {
-    if (padRect.left - paddle.xMovement >= gameBoardRect.left) {
-      paddle.start -= paddle.xMovement;
-    } else {
-      paddle.start -= padRect.left - gameBoardRect.left;
-    }
-    if (!game_started) {
-      ballDiv.style.transform = `translate(${paddle.start}px)`;
-      pad.style.transform = `translate(${paddle.start}px)`;
-      paddle.left = false;
-    }
-
-    pad.style.transform = `translate(${paddle.start}px)`;
-    paddle.left = false;
-  }
 }
 
 const ball = {
@@ -199,32 +132,6 @@ function ballWallCollision() {
     }
     livesbox.innerHTML = "&#10084".repeat(maxlives);
     game_started = false;
-  }
-}
-
-function padCollision() {
-  //also tried making the below two variables global but causes errors
-  const ballRect = ballDiv.getBoundingClientRect();
-  const padRect = pad.getBoundingClientRect();
-
-  //added + ballRect.height
-  if (ballRect.x < padRect.x + padRect.width && ballRect.x + ballRadius * 2 > padRect.x && ballRect.bottom >= padRect.top && ball.deltaY > 0) {
-
-    // play sound
-    if (game_started) {
-      paddle_hit_sound.play()
-    }
-    // CHECK WHERE THE ballRect HIT THE PADDLE
-    let collidePoint = ballRect.x - (padRect.x + padRect.width / 2);
-
-    // NORMALIZE THE VALUES
-    collidePoint = collidePoint / (padRect.width / 2);
-
-    // CALCULATE THE ANGLE OF THE ballRect
-    let angle = (collidePoint * Math.PI) / 3;
-
-    ball.deltaX = ball.speed * Math.sin(angle);
-    ball.deltaY = -ball.speed * Math.cos(angle);
   }
 }
 
@@ -534,3 +441,5 @@ function gameLoop() {
 
 
 requestAnimationFrame(gameLoop);
+
+export{ballDiv, ballRadius, game_started, ball}
---
+++ paddle.js
@@ -0,0 +1,115 @@
+import { ballDiv, ballRadius, game_started, ball} from "./game.js";
+
+
+//gameBoard variables
+// gameboard variables
+const gameBoard = document.querySelector(".game-board");
+const gameBoardRect = gameBoard.getBoundingClientRect();
+
+
+// paddle variables
+const paddle_width = 100;
+const paddle_margin_bottom = 50;
+const paddle_height = 20;
+
+const pad = document.createElement("div");
+const padCompStyles = window.getComputedStyle(pad);
+
+//sound
+const paddle_hit_sound = new Audio('./assets/paddlehit.mp3')
+
+
+let paddle = {
+  // start is used to track the translated values
+  start: 0,
+  x: gameBoardRect.width / 2 - paddle_width / 2,
+  // y value is the top left corner value of the paddle
+  y: gameBoardRect.height - paddle_margin_bottom - paddle_height,
+  width: paddle_width,
+  height: paddle_height,
+  xMovement: 20,
+  right: false,
+  left: false,
+};
+
+// draw paddle
+function drawPaddle() {
+    pad.classList.add("pad");
+    pad.style.left = paddle.x + "px";
+    pad.style.top = paddle.y + "px";
+    pad.style.width = paddle.width + "px";
+    pad.style.height = paddle.height + "px";
+    pad.style.backgroundColor = "white";
+    pad.style.position = "absolute";
+    gameBoard.append(pad);
+  }
+
+  
+//translates paddle on the X axis
+function movePaddle() {
+    const padRect = pad.getBoundingClientRect();
+  
+    if (paddle.right) {
+      if (padRect.right + paddle.xMovement <= gameBoardRect.right) {
+        paddle.start += paddle.xMovement;
+      } else {
+        paddle.start += gameBoardRect.right - padRect.right;
+      }
+  
+      if (!game_started) {
+        ballDiv.style.transform = `translateX(${paddle.start}px)`;
+        pad.style.transform = `translateX(${paddle.start}px)`;
+        paddle.right = false;
+      }
+  
+      pad.style.transform = `translate(${paddle.start}px)`;
+      paddle.right = false;
+    }
+  
+    if (paddle.left) {
+      if (padRect.left - paddle.xMovement >= gameBoardRect.left) {
+        paddle.start -= paddle.xMovement;
+      } else {
+        paddle.start -= padRect.left - gameBoardRect.left;
+      }
+      if (!game_started) {
+        ballDiv.style.transform = `translate(${paddle.start}px)`;
+        pad.style.transform = `translate(${paddle.start}px)`;
+        paddle.left = false;
+      }
+  
+      pad.style.transform = `translate(${paddle.start}px)`;
+      paddle.left = false;
+    }
+  }
+  
+  
+function padCollision() {
+    //also tried making the below two variables global but causes errors
+    const ballRect = ballDiv.getBoundingClientRect();
+    const padRect = pad.getBoundingClientRect();
+  
+    //added + ballRect.height
+    if (ballRect.x < padRect.x + padRect.width && ballRect.x + ballRadius * 2 > padRect.x && ballRect.bottom >= padRect.top && ball.deltaY > 0) {
+  
+      // play sound
+      if (game_started) {
+        paddle_hit_sound.play()
+      }
+      // CHECK WHERE THE ballRect HIT THE PADDLE
+      let collidePoint = ballRect.x - (padRect.x + padRect.width / 2);
+  
+      // NORMALIZE THE VALUES
+      collidePoint = collidePoint / (padRect.width / 2);
+  
+      // CALCULATE THE ANGLE OF THE ballRect
+      let angle = (collidePoint * Math.PI) / 3;
+  
+      ball.deltaX = ball.speed * Math.sin(angle);
+      ball.deltaY = -ball.speed * Math.cos(angle);
+    }
+  }
+  
+
+
+export{paddle, drawPaddle, movePaddle, padCollision, pad}
